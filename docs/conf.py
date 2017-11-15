b2b9dfaf4fc6544968c6615d25f67a9f0e383136
Rafael Cabañas de Paz <rafacabanas@gmail.com>
2017-11-15 12:06:58 +0100

    rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,9 @@
 import os
 import sys
 
+import sphinx_rtd_theme
+
+
 #sys.path.insert(0, os.path.abspath('../src/'))
 sys.path.insert(0, os.path.abspath('../inferpy'))
 
@@ -86,7 +89,19 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'bizstyle'
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    'rightsidebar': True,
+}
+
+
+#html_theme = 'bootstrap'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
+
+
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
