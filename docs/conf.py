8bffab8bf38a60a5ba394676fb645a6e8fd8b65f
Rafael Cabañas de Paz <rafacabanas@gmail.com>
2017-11-15 16:07:27 +0100

    readme.rst

--- docs/conf.py
+++ docs/conf.py
@@ -104,7 +104,6 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-# html_style_path = 'css/pytorch_theme.css'
 html_context = {
     'css_files': [
         '_static/css/inferpy_theme.css'
