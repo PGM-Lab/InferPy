68a5625604dfeac616e4ab874b5b720963bf898c
rcabanasdepaz <rafacabanas@gmail.com>
2018-04-23 15:34:54 +0200

    package bug

--- inferpy/__init__.py
+++ inferpy/__init__.py
@@ -4,7 +4,6 @@
 
 from inferpy import models
 from inferpy import util
-from inferpy.inferences.qmodel import *
 from inferpy.prob_model import *
 from inferpy.replicate import *
 from inferpy.version import *
