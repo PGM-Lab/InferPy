b5797c12dcd13f196d8e425d918a866c53eefab4
rcabanasdepaz <rafacabanas@gmail.com>
2018-05-04 16:37:30 +0200

    fixed

--- inferpy/inferences/inference.py
+++ inferpy/inferences/inference.py
@@ -1,4 +1,5 @@
-
+# -*- coding: utf-8 -*-
+#
 
 INF_METHODS = ["KLpq", "KLqp", "Laplace", "ReparameterizationEntropyKLqp",
                "ReparameterizationKLKLqp", "ReparameterizationKLqp", "ScoreEntropyKLqp",
