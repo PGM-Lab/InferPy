fd44f0ea163654710b63eccf0097131272be010b
jcozar87 <javier.cozar@uclm.es>
2019-04-03 10:18:58 +0200

    chore: included more comments for inference package

--- inferpy/inference/loss_functions/elbo.py
+++ inferpy/inference/loss_functions/elbo.py
@@ -5,11 +5,13 @@ from inferpy import util
 
 
 def ELBO(pmodel, qmodel, sample_dict):
-    # create combined model
+    # create combined model; for that first compute the plate size (for now, just one plate can be used)
     plate_size = pmodel._get_plate_size(sample_dict)
 
+    # expand the qmodel
     qvars, _ = qmodel.expand_model(plate_size)
 
+    # expand de pmodel, using the intercept.set_values function, to include the sample_dict and the expanded qvars
     with ed.interception(util.interceptor.set_values(**{**qvars, **sample_dict})):
         pvars, _ = pmodel.expand_model(plate_size)
 
