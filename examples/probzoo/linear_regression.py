a892d6edc2a7b3eb329b3f3b5541578cec194ee7
rafa <rafacabanas@gmail.com>
2019-05-19 18:48:12 +0200

    feat: RV copy method (with fixed parameters)

--- examples/probzoo/linear_regression.py
+++ examples/probzoo/linear_regression.py
@@ -1,27 +1,47 @@
-import numpy as np
-import inferpy as inf
-N=1000
-x_train = np.concatenate([inf.Normal([0.0,0.0], scale=1.).sample(int(N/2)), inf.Normal([10.0,10.0], scale=1.).sample(int(N/2))])
-##########
-
-
 # required pacakges
 import inferpy as inf
 import numpy as np
 import tensorflow as tf
 
-# definition of a generic model
+
 @inf.probmodel
 def linear_reg(d):
-    w0 = inf.Normal(0,1, name="w0")
-    w = inf.Normal(0, 1, batch_shape=[d])
+    w0 = inf.Normal(0, 1, name="w0")
+    w = inf.Normal(0, 1, batch_shape=[d, 1], name="w")
 
     with inf.datamodel():
-        z = inf.Normal(tf.ones([k]),1, name="z")       # shape = [N,k]
-        x = inf.Normal(z @ beta , 1, name="x")         # shape = [N,d]
+        x = inf.Normal(5, 2, batch_shape=d, name="x")
+        y = inf.Normal(w0 + x @ w, 1.0, name="y")
+
+@inf.probmodel
+def qmodel(d):
+    qw0_loc = inf.Parameter(0., name="qw0_loc")
+    qw0_scale = tf.math.softplus(inf.Parameter(1., name="qw0_scale"))
+    qw0 = inf.Normal(qw0_loc, qw0_scale, name="qw0")
+
+    qw_loc = inf.Parameter(tf.zeros([d,1]), name="qw_loc")
+    qw_scale = tf.math.softplus(inf.Parameter(tf.ones([d,1]), name="qw_scale"))
+    qbeta = inf.Normal(qw_loc, qw_scale, name="w")
 
 
 # create an instance of the model
+m = linear_reg(d=2)
 
-d = 5
+### create toy data
+N = 1000
+data = m.sample(size = N, data={"w0":0, "w":[[2],[1]]})
+x_train = data["x"]
+y_train = data["y"]
+
+
+
+VI = inf.inference.VI(qmodel(2), epochs=10000)
+m.fit({"x": x_train, "y":y_train}, VI)
+
+sess = inf.get_session()
+print(m.posterior["w"].sample())
+print(sess.run(m.posterior["w"].loc))
+
+with tf.Session() as sess2:
+    print(sess2.run(m.posterior["w"].copy().loc))
 
