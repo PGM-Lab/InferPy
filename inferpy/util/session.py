70d34d280200b2fd85efdac662e43c3279392cc5
jcozar87 <javier.cozar@uclm.es>
2019-06-04 09:54:18 +0200

    feat: new_session function to allow to create a new default session using GPU

--- inferpy/util/session.py
+++ inferpy/util/session.py
@@ -10,6 +10,16 @@ Module to manage global sessions and graphs executed in sessions
 __session = None
 
 
+def new_session(gpu_memory_fraction=0.0):
+    # Create a new session. By default do not use GPU. Use gpu_memory_fraction > 0 (and <= 1) to use GPU.
+    if gpu_memory_fraction <= 0.0:
+        set_session(tf.Session())
+    else:
+        config = tf.ConfigProto(log_device_placement=True)
+        config.gpu_options.per_process_gpu_memory_fraction = gpu_memory_fraction  # memory_fraction [0-1]
+        set_session(tf.Session(config=config))
+
+
 def get_session():
     global __session
     if not __session:
